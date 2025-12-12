eba762c06d5e44c87556f7c87b9d2e37e4bc3f05
choudharyaanchal95-del <choudhary.aanchal95@gmail.com>
2025-12-12 16:49:33 +0530

    Add files via upload

---
+++ Task 2 - Image Encrypt.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+def encrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r, g, b = pixels[i, j]
+            pixels[i, j] = ((r + key) % 256, (g + key) % 256, (b + key) % 256)
+
+    img.save(output_path)
+    print("Image encrypted successfully!")
+
+def decrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r, g, b = pixels[i, j]
+            pixels[i, j] = ((r - key) % 256, (g - key) % 256, (b - key) % 256)
+
+    img.save(output_path)
+    print("Image decrypted successfully!")
